0e45fa7aafa2f45faa8b8efd3c8d61e29cba4939
Meszaros Nandor Farkas <farkas.nandor.meszaros@gmail.com>
2025-01-12 17:05:56 +0100

    ciklus bevezetese

--- main.js
+++ main.js
@@ -8,29 +8,29 @@ const fejlec  = [
 ]
 
 const array = [ 
-    { //array masodik elem létrehozása az elsosor
-        oszlop1: "Optika", //az array masodik elem oszlop1 tulajdonság ertekadása
-        oszlop2: "XI. század", //az array masodik elem oszlop2 tulajdonság ertekadása
-        oszlop3: "Alhazen", //az array masodik elem oszlop3 tulajdonság ertekadása
+    { //array elso elem létrehozása az elsosor
+        sor1: "Optika", //az array elso elem sor1 tulajdonság ertekadása
+        sor2: "XI. század", //az array elso elem sor2 tulajdonság ertekadása
+        sor3: "Alhazen", //az array elso elem sor3 tulajdonság ertekadása
     },
-    { //array harmadik elem létrehozása a masodiksor
-        oszlop1: "Asztronómia", //az array harmadik elem oszlop1 tulajdonság ertekadása
-        oszlop2: "reneszánsz", //az array harmadik elem oszlop2 tulajdonság ertekadása
-        oszlop3: "Kepler", //az array harmadik elem oszlop3 tulajdonság ertekadása
-        oszlop4: "Galilei", //az array harmadik elem oszlop4 tulajdonság ertekadása
+    { //array masodik elem létrehozása a masodiksor
+        sor1: "Asztronómia", //az array masodik elem sor1 tulajdonság ertekadása
+        sor2: "reneszánsz", //az array masodik elem sor2 tulajdonság ertekadása
+        sor3: "Kepler", //az array masodik elem sor3 tulajdonság ertekadása
+        sor4: "Galilei", //az array masodik elem sor4 tulajdonság ertekadása
     },
-    { //array negyedik elem létrehozása a harmadiksor
-        oszlop1: "Kvantumfizika", //az array negyedik elem oszlop1 tulajdonság ertekadása
-        oszlop2: "XIX-XX. század	", //az array negyedik elem oszlop2 tulajdonság ertekadása
-        oszlop3: "Max Planck", //az array negyedik elem oszlop3 tulajdonság ertekadása
-        oszlop4: "Albert Einstei", //az array negyedik elem oszlop4 tulajdonság ertekadása
+    { //array harmadik elem létrehozása a harmadiksor
+        sor1: "Kvantumfizika", //az array harmadik elem sor1 tulajdonság ertekadása
+        sor2: "XIX-XX. század	", //az array harmadik elem sor2 tulajdonság ertekadása
+        sor3: "Max Planck", //az array harmadik elem sor3 tulajdonság ertekadása
+        sor4: "Albert Einstei", //az array harmadik elem sor4 tulajdonság ertekadása
 
     },
-    { //array ötödik elem létrehozása a negyediksor
-        oszlop1: "Modern fizika	", //az array ötödik elem oszlop1 tulajdonság ertekadása
-        oszlop2: "XX-XXI. század	", //az array ötödik elem oszlop2 tulajdonság ertekadása
-        oszlop3: "Richard Feynman	", //az array ötödik elem oszlop3 tulajdonság ertekadása
-        oszlop4: "Stephen Hawking", //az array ötödik elem oszlop4 tulajdonság ertekadása
+    { //array negyedik elem létrehozása a negyediksor
+        sor1: "Modern fizika	", //az array negyedik elem sor1 tulajdonság ertekadása
+        sor2: "XX-XXI. század	", //az array negyedik elem sor2 tulajdonság ertekadása
+        sor3: "Richard Feynman	", //az array negyedik elem sor3 tulajdonság ertekadása
+        sor4: "Stephen Hawking", //az array negyedik elem sor4 tulajdonság ertekadása
 
     },
     
@@ -61,20 +61,22 @@ for(let i = 0; i < array.length; i++ ) { //végigiterálok egy for ciklussal az 
     tablazat.appendChild(sor) //a fő táblázathoz hozzácsatolom a sort
 
     const elsosor = document.createElement('td') //elsosor létrehozása
-    elsosor.innerHTML = mostanielement.oszlop1 //elsosor innerHTML-je a az array-ben a mostanielement (i)-nek a oszlop1.je
+    elsosor.innerHTML = mostanielement.sor1 //elsosor innerHTML-je a az array-ben a mostanielement (i)-nek a sor1.je
     sor.appendChild(elsosor) //hozzátesszük a sorhoz az elso oszlop elso elemjét 
 
     const masodiksor = document.createElement('td') //masodiksor létrehozása
-    masodiksor.innerHTML = mostanielement.oszlop2 //masodiksor innerHTML-je a az array-ben a mostanielement (i)-nek a oszlop2.je
+    masodiksor.innerHTML = mostanielement.sor2 //masodiksor innerHTML-je a az array-ben a mostanielement (i)-nek a sor2.je
     sor.appendChild(masodiksor) //hozzátesszük a sorhoz az elso oszlop masodik elemjét 
 
     const harmadiksor = document.createElement('td') //harmadiksor létrehozása
-    harmadiksor.innerHTML = mostanielement.oszlop3 //harmadiksor innerHTML-je a az array-ben a mostanielement (i)-nek a oszlop3.je
+    harmadiksor.innerHTML = mostanielement.sor3 //harmadiksor innerHTML-je a az array-ben a mostanielement (i)-nek a sor3.je
+    harmadiksor.colSpan = 2 //beállítjuk a colspant.
     sor.appendChild(harmadiksor) //hozzátesszük a sorhoz az elso oszlop harmadik elemjét 
 
     const negyediksor = document.createElement('td') //negyediksor létrehozása
-    negyediksor.innerHTML = mostanielement.oszlop4 //negyediksor innerHTML-je a az array-ben a mostanielement (i)-nek a oszlop4.je
+    negyediksor.innerHTML = mostanielement.sor4 //negyediksor innerHTML-je a az array-ben a mostanielement (i)-nek a sor4.je
     sor.appendChild(negyediksor) //hozzátesszük a sorhoz az elso oszlop negyedik elemjét 
     tbody.appendChild(sor)
+   
 }
 
