ea4c10d04900b71dc325f7bcdf7f8424c291242f
Meszaros Nandor Farkas <farkas.nandor.meszaros@gmail.com>
2025-01-15 00:30:11 +0100

    esemenykezelo bevezetese

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 const fejlec  = [
     //headerarray létrehozása
     { //array első elem létrehozása a fejléc
-        fejlec1: "Fizika terület", //az array elso elem fejlec1 tulajdonság ertekadása
-        fejlec2: "Időszak", //az array elso elem fejlec2 tulajdonság ertekadása
-        fejlec3: "Képviselők" //az array elso elem fejlec3 tulajdonság ertekadása
+        elso: "Fizika terület", //az array elso elem fejlec1 tulajdonság ertekadása
+        masodik: "Időszak", //az array elso elem fejlec2 tulajdonság ertekadása
+        harmadik: "Képviselők", //az array elso elem fejlec3 tulajdonság ertekadása
     },
 ]
 
 const array = [ 
     { //array elso elem létrehozása az elsosor
-        sor1: "Optika", //az array elso elem sor1 tulajdonság ertekadása
-        sor2: "XI. század", //az array elso elem sor2 tulajdonság ertekadása
-        sor3: "Alhazen", //az array elso elem sor3 tulajdonság ertekadása
+        fizikaterulet: "Optika", //az array elso elem sor1 tulajdonság ertekadása
+        idoszak: "XI. század", //az array elso elem sor2 tulajdonság ertekadása
+        tudos1: "Alhazen", //az array elso elem sor3 tulajdonság ertekadása
     },
     { //array masodik elem létrehozása a masodiksor
-        sor1: "Asztronómia", //az array masodik elem sor1 tulajdonság ertekadása
-        sor2: "reneszánsz", //az array masodik elem sor2 tulajdonság ertekadása
-        sor3: "Kepler", //az array masodik elem sor3 tulajdonság ertekadása
-        sor4: "Galilei", //az array masodik elem sor4 tulajdonság ertekadása
+        fizikaterulet: "Asztronómia", //az array masodik elem sor1 tulajdonság ertekadása
+        idoszak: "reneszánsz", //az array masodik elem sor2 tulajdonság ertekadása
+        tudos1: "Kepler", //az array masodik elem sor3 tulajdonság ertekadása
+        tudos2: "Galilei", //az array masodik elem sor4 tulajdonság ertekadása
     },
     { //array harmadik elem létrehozása a harmadiksor
-        sor1: "Kvantumfizika", //az array harmadik elem sor1 tulajdonság ertekadása
-        sor2: "XIX-XX. század	", //az array harmadik elem sor2 tulajdonság ertekadása
-        sor3: "Max Planck", //az array harmadik elem sor3 tulajdonság ertekadása
-        sor4: "Albert Einstei", //az array harmadik elem sor4 tulajdonság ertekadása
+        fizikaterulet: "Kvantumfizika", //az array harmadik elem sor1 tulajdonság ertekadása
+        idoszak: "XIX-XX. század	", //az array harmadik elem sor2 tulajdonság ertekadása
+        tudos1: "Max Planck", //az array harmadik elem sor3 tulajdonság ertekadása
+        tudos2: "Albert Einstei", //az array harmadik elem sor4 tulajdonság ertekadása
 
     },
     { //array negyedik elem létrehozása a negyediksor
-        sor1: "Modern fizika	", //az array negyedik elem sor1 tulajdonság ertekadása
-        sor2: "XX-XXI. század	", //az array negyedik elem sor2 tulajdonság ertekadása
-        sor3: "Richard Feynman	", //az array negyedik elem sor3 tulajdonság ertekadása
-        sor4: "Stephen Hawking", //az array negyedik elem sor4 tulajdonság ertekadása
+        fizikaterulet: "Modern fizika	", //az array negyedik elem sor1 tulajdonság ertekadása
+        idoszak: "XX-XXI. század	", //az array negyedik elem sor2 tulajdonság ertekadása
+        tudos1: "Richard Feynman	", //az array negyedik elem sor3 tulajdonság ertekadása
+        tudos2: "Stephen Hawking", //az array negyedik elem sor4 tulajdonság ertekadása
 
     },
     
@@ -45,22 +45,22 @@ header.appendChild(tr) //a tr-t hozzátesszük a headerünkhöz (thead)
 const tbody = document.createElement('tbody') //csinálunk egy tbodyt
 tablazat.appendChild(tbody) //a tbody-t pedig a táblázatunkhoz fűzünk.
 
+for (let adat in fejlec[0]) { // for ciklussal az adatokat nézzük a fejléc első eleméből
+    const th = document.createElement('th'); // létrehozunk egy th elemet
+    th.innerHTML = fejlec[0][adat] // a th elem belső HTML-jét az aktuális adat értékére állítjuk
+    tr.appendChild(th); // hozzáadjuk a sorhoz
 
+    // Feltételezzük, hogy a fejlec[0] tartalmazza a fejlec3-at, amelyhez colSpan-t kell beállítani
+    if (adat === 'harmadik') { 
+        th.colSpan = 2; // colSpan beállítása a th elemre
+    }
+}
 
 
 
 function tablegenerate() {
 
-    for (let adat in fejlec[0]) { // for ciklussal az adatokat nézzük a fejléc első eleméből
-        const th = document.createElement('th'); // létrehozunk egy th elemet
-        th.innerHTML = fejlec[0][adat];; // a th elem belső HTML-jét az aktuális adat értékére állítjuk
-        tr.appendChild(th); // hozzáadjuk a sorhoz
     
-        // Feltételezzük, hogy a fejlec[0] tartalmazza a fejlec3-at, amelyhez colSpan-t kell beállítani
-        if (adat === 'fejlec3') { 
-            th.colSpan = 2; // colSpan beállítása a th elemre
-        }
-    }
     for(let i = 0; i < array.length; i++ ) { //végigiterálok egy for ciklussal az array-en
         const mostanielement = array[i]  //az i-dik element a mostanielement lesz
         const sor = document.createElement('tr') // csinálok egy sort
@@ -68,24 +68,26 @@ function tablegenerate() {
         tablazat.appendChild(sor) //a fő táblázathoz hozzácsatolom a sort
     
         const elsosor = document.createElement('td') //elsosor létrehozása
-        elsosor.innerHTML = mostanielement.sor1 //elsosor innerHTML-je a az array-ben a mostanielement (i)-nek a sor1.je
+        elsosor.innerHTML = mostanielement.fizikaterulet //elsosor innerHTML-je a az array-ben a mostanielement (i)-nek a sor1.je
         sor.appendChild(elsosor) //hozzátesszük a sorhoz az elso oszlop elso elemjét 
     
         const masodiksor = document.createElement('td') //masodiksor létrehozása
-        masodiksor.innerHTML = mostanielement.sor2 //masodiksor innerHTML-je a az array-ben a mostanielement (i)-nek a sor2.je
+        masodiksor.innerHTML = mostanielement.idoszak //masodiksor innerHTML-je a az array-ben a mostanielement (i)-nek a sor2.je
         sor.appendChild(masodiksor) //hozzátesszük a sorhoz az elso oszlop masodik elemjét 
     
         const harmadiksor = document.createElement('td') //harmadiksor létrehozása
-        harmadiksor.innerHTML = mostanielement.sor3 //harmadiksor innerHTML-je a az array-ben a mostanielement (i)-nek a sor3.je
+        harmadiksor.innerHTML = mostanielement.tudos1 //harmadiksor innerHTML-je a az array-ben a mostanielement (i)-nek a sor3.je
         if (i === 0) { 
             harmadiksor.colSpan = 2 // colSpan bellítása
         }
-        
+        if (!mostanielement.tudos2) {
+            harmadiksor.colSpan = 2 // ha nincs tudos2 akkor colspan 2
+        }
         sor.appendChild(harmadiksor) //hozzátesszük a sorhoz az elso oszlop harmadik elemjét 
         
         if( i > 0 ) { // ugye ha az első itrácion végig ment akkor már nyugodtan lehet negyedik sor
             const negyediksor = document.createElement('td') //negyediksor létrehozása
-            negyediksor.innerHTML = mostanielement.sor4 //negyediksor innerHTML-je a az array-ben a mostanielement (i)-nek a sor4.je
+            negyediksor.innerHTML = mostanielement.tudos2 //negyediksor innerHTML-je a az array-ben a mostanielement (i)-nek a sor4.je
             sor.appendChild(negyediksor) //hozzátesszük a sorhoz az elso oszlop negyedik elemjét 
     
         }
@@ -97,3 +99,39 @@ function tablegenerate() {
 
 tablegenerate()
 
+const form = document.getElementById('form') // megszerezzük az id alapján a formot
+
+form.addEventListener('submit', function(e) {
+
+    e.preventDefault(); //az alapértelmezett böngészű lefusson megakadályozza
+    const terulet = document.getElementById("fizika") //elkerem a fizika idt és beteszem az itt létrehozott változóba
+    const idoszak = document.getElementById("ido") //elkerem a ido idt és beteszem az itt létrehozott változóba
+    const tudos1 = document.getElementById("tudos1") //elkerem a tudos1 idt és beteszem az itt létrehozott változóba
+    const tudos2 = document.getElementById("tudos2") //elkerem a tudos2 idt és beteszem az itt létrehozott változóba
+    
+    
+    const teruletertek = terulet.value //itt egy másik változóba belerakom az elöbb elkért terulet változó értékét
+
+    const idoszakertek = idoszak.value //itt egy másik változóba belerakom az elöbb elkért idoszak változó értékét
+
+    const tudos1ertek = tudos1.value //itt egy másik változóba belerakom az elöbb elkért tudos1 változó értékét
+
+    const tudos2ertek = tudos2.value//itt egy másik változóba belerakom az elöbb elkért tudos2 változó értékét
+    
+    
+
+    const ujadat = { // egy uj objektumot hozunk létre 
+        fizikaterulet: teruletertek, // az uj fizikateruletnek a teruletertek lesz az értéke
+        idoszak : idoszakertek, // az idoszaknak az idoszakerteke lesz az értéke
+        tudos1 : tudos1ertek, // a tudos1-nek a tudos1ertek lesz az új értéke
+        tudos2 : tudos2ertek // a tudos2-nek a tudos2ertek lesz az új értéke
+        
+    }
+
+
+    array.push(ujadat) //belerakjuk az arrayben ami ugye már létezik ezt az új létrehozott adatobjektumunkat.
+
+    tbody.innerHTML = "" //kitörlöm a táblázatot azért a tbodyt mert abban van igazából az egész táblázat a headerrel nem kell foglalkozni.
+
+    tablegenerate() // és az ujonnan belerakott dologgal ujragenerálom a táblát.
+})
