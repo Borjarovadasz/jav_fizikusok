64b6457ffd366987b89f02bf75532a514f31b73a
Meszaros Nandor Farkas <farkas.nandor.meszaros@gmail.com>
2025-02-14 15:05:29 +0100

    form generalas fuggveny bevezetese

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-const fejlec  = 
+const fejlec  = //létrehozunk egy fejléc arrayt objektumokkal
     [
         {
-            szoveg: "Fizika terület"
+            szoveg: "Fizika terület" //elso objektomunk amiben eltároljuk a szöveget
         },
         {
-            szoveg: "Időszak"
+            szoveg: "Időszak" //második objektomunk amiben eltároljuk a szöveget
         },
         {
-            szoveg: "Képviselők", colSpan: 2
+            szoveg: "Képviselők", colSpan: 2 //harmadik objektomunk amiben eltároljuk a szöveget és adunk neki colspan-2 is
         }
     ]
 
-    const form1 = [
-        {
-            label: "Terület megnevezése",
-            id: "fizika",
-            for:"fizika"
+    const form1 = [ //létrehozunk egy fejléc arrayt objektumokkal
+        { //elso objektumunk
+            label: "Terület megnevezése", //adunk egy labelt és egy értéket hozzá amit majd használunk a form generálásnál
+            id: "fizika", //adunk egy idt és egy értéket hozzá amit majd használunk a form generálásnál
+            for:"fizika" //adunk egy for-t és egy értéket hozzá amit majd használunk a form generálásnál
         },
-        {
-            label: "Időszak",
-            id: "ido",
-            for:"ido"
+        { //második objektumunk
+            label: "Időszak", //adunk egy és egy értéket hozzá amit majd használunk a form generálásnál
+            id: "ido", //adunk egy idt és egy értéket hozzá amit majd használunk a form generálásnál
+            for:"ido" //adunk egy for-t és egy értéket hozzá amit majd használunk a form generálásnál
         },
-        {
-            label: "Első Tudos:",
-            id: "tudos1",
-            for:"tudos1"
+        { //harmadik objektumunk
+            label: "Első Tudos:", //adunk egy labelt és egy értéket hozzá amit majd használunk a form generálásnál
+            id: "tudos1", //adunk egy id-t és egy értéket hozzá amit majd használunk a form generálásnál
+            for:"tudos1" //adunk egy for-t és egy értéket hozzá amit majd használunk a form generálásnál
         },
-        {
-            label: "Második Tudos:",
-            id: "tudos2",
-            for:"tudos1"
+        { //negyedik objektumunk
+            label: "Második Tudos:", //adunk egy labelt és egy értéket hozzá amit majd használunk a form generálásnál
+            id: "tudos2", //adunk egy id-t és egy értéket hozzá amit majd használunk a form generálásnál
+            for:"tudos1" //adunk egy for-t és egy értéket hozzá amit majd használunk a form generálásnál
         }
     ]
 
@@ -65,44 +65,44 @@ const array = [
     
 ]
 
-function formgenerate() {
-    const form = document.createElement('form')
-    form.id = "form"
-   for(let i = 0; i < form1.length; i++) {
-        const div = document.createElement('div')
-        const label = document.createElement('label')
-        label.htmlFor  = form1[i].for
-        label.innerHTML = form1[i].label
+function formgenerate() { //formgenerate függvény létrehozása
+    const form = document.createElement('form') //csinálom egy form változó alatt egy formot
+    form.id = "form" // a form id-je legyen form
+   for(let i = 0; i < form1.length; i++) { //végig iterálunk a form1 arrayünkön
+        const div = document.createElement('div') //csinálunk egy div-et div névvel
+        const label = document.createElement('label') //csinálunk egy labelt-
+        label.htmlFor  = form1[i].for //a label-nek a "fos"-os tulajdonság értéke az legyen mindig az adott ciklus i-nek az arrayben a "for"-hoz hozzákapcsolt érték.
+        label.innerHTML = form1[i].label //a label-nek legyen az innerhtml-je az adott ciklis i-nek az arrayban a "label"-hez kapcsolt érték.
 
-        const input = document.createElement('input')
-        input.type = 'text'
-        input.id = form1[i].id
-        input.name = form1[i].id
+        const input = document.createElement('input') //csinálunk egy inputot
+        input.type = 'text' //az input typeja legyen text
+        input.id = form1[i].id //az input id-je legyen mindig az arrayban az id tulajdonság mögötti értékek valamelyik attól függ hol tartunk az iterácion. 
+        input.name = form1[i].id //az input name-je legyen szintén ugyan az mint az id mert az ugyan az.
 
-        const br = document.createElement('br') 
+        const br = document.createElement('br')  // csinálunk egy br-t hogy ne legyn csúnya és ne legyen olyan közel egymáshoz minden
 
-        div.appendChild(label)
-        div.appendChild(br)
-        div.appendChild(input)
+        div.appendChild(label) //a fődivunkhöz hozzátesszük a labelt amiben már van sok minden (for, innerhtml)
+        div.appendChild(br) //a fődivunkhöz hozzátesszük a br-t hogy itt mér tényleg lássuk hogy szép lesz
+        div.appendChild(input) //a fődivunkhöz az inputot amiben itt is már van (id,name,type)
         
         
-        const errordiv = document.createElement('div')
-        errordiv.className = 'error'
-        div.appendChild(errordiv)
+        const errordiv = document.createElement('div') //csináluk egy divet errordiv névvel
+        errordiv.className = 'error' //az errordivnek a classneve az legyen "error"
+        div.appendChild(errordiv) //majd szépen hozzátesszük a fődivhez ezt az uj divet. Persze mivel egy forcikluson belül vagyunk ebből is sok lesz vagyis amilyen hosszú az arrayünk.
 
  
        
-        form.appendChild(div)
+        form.appendChild(div) //majd végül a fődivet amiben van már label, input, br, errordiv is hozzátesszük a formhoz.
        
     
    }
-   const button = document.createElement('button')
-   button.innerHTML = "Hozzáadás"
-   document.body.appendChild(form)
-   form.appendChild(button)
+   const button = document.createElement('button') //csinálunk egy gombot
+   button.innerHTML = "Hozzáadás" //a gombnak legyen az innerhtml-je a "Hozzáadás"
+   document.body.appendChild(form) //a bodyhoz hozzátesszük a formot
+   form.appendChild(button) //majd a gombot pedig a formhoz
 }
 
-formgenerate()
+formgenerate() //majd meghívjuk ezt a csodálatos függvényt
 
 const tablazat = document.createElement('table') // Létrehozzuk a táblázat változo
 document.body.appendChild(tablazat) // hozzá appendáljuk a táblázatot a bodyhoz
